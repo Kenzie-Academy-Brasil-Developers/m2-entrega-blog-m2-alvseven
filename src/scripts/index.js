199002cc234b86a2389262f01fd2b302c800d19c
alvseven <you@example.com>
2022-07-10 12:36:36 -0300

    removendo os comentários

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -34,34 +34,3 @@ class Cadastrar {
 
 Cadastrar.fazerCadastro()
 
-// async function listenerCadastro () {
-
-//     const btn = document.querySelector(".btn-cadastrar")
-
-//     btn.addEventListener("click", async (event) => {
-//         event.preventDefault()
-
-//         const nome = document.querySelector(".input-nome").value
-//         const email = document.querySelector(".input-email").value
-//         const fotoPerfil = document.querySelector(".input-foto").value
-//         const senha = document.querySelector(".input-senha").value
-    
-//         const data = {
-//             username: nome, 
-//             email: email,
-//             avatarUrl: fotoPerfil,
-//             password: senha
-//         }
-    
-//         const cadastro = await Cadastro.cadastrar(data)
-//         const url = (window.location.href).split("/")
-//         const filteredUrl = url.slice(0, 3).join("/")
-
-//         if (cadastro.id) {
-//            window.location.href = `${filteredUrl}/m2-entrega-blog-m2-alvseven/src/pages/login.html`
-//         }
-//     })
-
-// }
-
-// listenerCadastro()
--- src/scripts/loginpage.js
+++ src/scripts/loginpage.js
@@ -20,32 +20,9 @@ class Loginpage {
             if (this.userId && this.token) {
                 window.location.href = `${this.filteredUrl}/m2-entrega-blog-m2-alvseven/src/pages/homepage.html`
             }
-    
+
         })
     }
 }
 
 Loginpage.fazerLogin()
-
-// function fazerLogin () {
-//     const btn = document.querySelector(".btn-login")
-//     btn.addEventListener("click", async (event) => {
-//         event.preventDefault()
-
-//         const email = document.querySelector(".input-email").value
-//         const password = document.querySelector(".input-senha").value
-//         await Login.logar(email, password)
-
-//         const userId = localStorage.getItem("@kenzie-blog:userId")
-//         const token  = localStorage.getItem("@kenzie-blog:token")
-//         const url = (window.location.href).split("/")
-//         const filteredUrl = url.slice(0, 3).join("/")
-
-//         if (userId && token) {
-//             window.location.href = `${filteredUrl}/m2-entrega-blog-m2-alvseven/src/pages/homepage.html`
-//         }
-
-//     })
-// }
-
-// fazerLogin()
